5a7151ed8847de69e8fdeac25fcd9601b8f0e9c5
reazsana9 <reazsana9@gmail.com>
2026-07-24 00:51:33 +0600

    path error solved

--- script/login.js
+++ script/login.js
@@ -17,7 +17,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
         alert("login Success");
 
         // window.location.replace("../home.html")  
-        window.location.assign("/home.html");
+        window.location.assign("./home.html");
     } else {
         // 3-2 false:::>> alert> return
         alert("login Failed");
